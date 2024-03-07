339e5228c9fea4909b86a54c7b9632f8fd9520b3
Rwandantechy <niyinnocent2027@gmail.com>
2024-03-07 15:49:53 +0530

    changed the advocates

--- assets/js/main.js
+++ assets/js/main.js
@@ -156,3 +156,30 @@
   themeCheck();
   /* ========  themeSwitcher End ========= */
 })();
+
+// Testimonial
+const testimonialSwiper = new Swiper(".testimonial-carousel", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+    1280: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+  },
+});
